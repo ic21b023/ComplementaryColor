f3df198e72207939eb650c45b2aa01f7329ef1c2
Roman REITER <reiter@live.at>
2022-12-06 18:34:35 +0100

    #1 init project

---
+++ src/test/java/at/technikum/ComplementaryColor/integrationTests/ComplementaryColorIT.java
@@ -0,0 +1,13 @@
+package at.technikum.ComplementaryColor.integrationTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ComplementaryColorIT {
+
+    @Test
+    public void Dummy(){
+        assertTrue(1==1);
+    }
+}
---
+++ src/test/java/at/technikum/ComplementaryColor/unitTests/ComplementaryColorUnitTests.java
@@ -0,0 +1,13 @@
+package at.technikum.ComplementaryColor.unitTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ComplementaryColorUnitTests{
+
+    @Test
+    public void Dummy(){
+        assertTrue(1==1);
+    }
+}

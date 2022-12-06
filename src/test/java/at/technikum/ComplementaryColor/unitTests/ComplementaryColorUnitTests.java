826cec777046e352c55b9812e6835618f2d6d5d3
Roman REITER <reiter@live.at>
2022-12-06 18:58:44 +0100

    #3 implement unit tests

--- src/test/java/at/technikum/ComplementaryColor/unitTests/ComplementaryColorUnitTests.java
+++ src/test/java/at/technikum/ComplementaryColor/unitTests/ComplementaryColorUnitTests.java
@@ -1,13 +1,17 @@
 package at.technikum.ComplementaryColor.unitTests;
 
+import at.technikum.ComplementaryColor.service.ColorService;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ComplementaryColorUnitTests{
 
+    ColorService colorService = new ColorService();
+
     @Test
-    public void Dummy(){
-        assertTrue(1==1);
+    public void testGetComplementaryColor(){
+        assertEquals("orange",colorService.getComplementaryColor("blue"));
     }
 }
